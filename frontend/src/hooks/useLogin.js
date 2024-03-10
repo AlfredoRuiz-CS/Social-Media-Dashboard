51d4ffe910ded61f9a1427154d906db49aaae42a
AlfredoRuiz-CS <alfreruiz2210@gmail.com>
2024-03-09 22:31:01 -0600

    cors fixes

--- frontend/src/hooks/useLogin.js
+++ frontend/src/hooks/useLogin.js
@@ -12,6 +12,7 @@ export const useLogin = () => {
 
     const response = await fetch('https://social-media-dashboard-five-livid.vercel.app/api/user/login', {
       method: 'POST',
+      mode: 'cors',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({ email, password }),
     })
